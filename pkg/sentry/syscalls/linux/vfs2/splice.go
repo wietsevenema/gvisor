632c6b85a8678c267aa7879f61ff53c83074ffa8
Nicolas Lacasse <nlacasse@google.com>
2020-06-24 16:55:57 -0700

    Port sendfile to vfs2.
    And do some refactoring of the wait logic in sendfile/splice/tee.
    
    Updates #1035 #2923
    
    PiperOrigin-RevId: 318168827

--- pkg/sentry/syscalls/linux/vfs2/splice.go
+++ pkg/sentry/syscalls/linux/vfs2/splice.go
@@ -15,12 +15,15 @@
 package vfs2
 
 import (
+	"io"
+
 	"gvisor.dev/gvisor/pkg/abi/linux"
 	"gvisor.dev/gvisor/pkg/sentry/arch"
 	"gvisor.dev/gvisor/pkg/sentry/kernel"
 	"gvisor.dev/gvisor/pkg/sentry/kernel/pipe"
 	"gvisor.dev/gvisor/pkg/sentry/vfs"
 	"gvisor.dev/gvisor/pkg/syserror"
+	"gvisor.dev/gvisor/pkg/usermem"
 	"gvisor.dev/gvisor/pkg/waiter"
 )
 
@@ -110,10 +113,8 @@ func Splice(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.Syscal
 
 	// Move data.
 	var (
-		n     int64
-		err   error
-		inCh  chan struct{}
-		outCh chan struct{}
+		n   int64
+		err error
 	)
 	for {
 		// If both input and output are pipes, delegate to the pipe
@@ -142,33 +143,8 @@ func Splice(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.Syscal
 			break
 		}
 
-		// Note that the blocking behavior here is a bit different than the
-		// normal pattern. Because we need to have both data to read and data
-		// to write simultaneously, we actually explicitly block on both of
-		// these cases in turn before returning to the splice operation.
-		if inFile.Readiness(eventMaskRead)&eventMaskRead == 0 {
-			if inCh == nil {
-				inCh = make(chan struct{}, 1)
-				inW, _ := waiter.NewChannelEntry(inCh)
-				inFile.EventRegister(&inW, eventMaskRead)
-				defer inFile.EventUnregister(&inW)
-				continue // Need to refresh readiness.
-			}
-			if err = t.Block(inCh); err != nil {
-				break
-			}
-		}
-		if outFile.Readiness(eventMaskWrite)&eventMaskWrite == 0 {
-			if outCh == nil {
-				outCh = make(chan struct{}, 1)
-				outW, _ := waiter.NewChannelEntry(outCh)
-				outFile.EventRegister(&outW, eventMaskWrite)
-				defer outFile.EventUnregister(&outW)
-				continue // Need to refresh readiness.
-			}
-			if err = t.Block(outCh); err != nil {
-				break
-			}
+		if err = waitForBoth(t, inFile, outFile); err != nil {
+			break
 		}
 	}
 
@@ -246,10 +222,6 @@ func Tee(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallCo
 	}
 
 	// Copy data.
-	var (
-		inCh  chan struct{}
-		outCh chan struct{}
-	)
 	for {
 		n, err := pipe.Tee(t, outPipeFD, inPipeFD, count)
 		if n != 0 {
@@ -258,34 +230,185 @@ func Tee(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallCo
 		if err != syserror.ErrWouldBlock || nonBlock {
 			return 0, nil, err
 		}
+		if err = waitForBoth(t, inFile, outFile); err != nil {
+			return 0, nil, err
+		}
+	}
+}
 
-		// Note that the blocking behavior here is a bit different than the
-		// normal pattern. Because we need to have both data to read and data
-		// to write simultaneously, we actually explicitly block on both of
-		// these cases in turn before returning to the tee operation.
+// Sendfile implements linux system call sendfile(2).
+func Sendfile(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallControl, error) {
+	outFD := args[0].Int()
+	inFD := args[1].Int()
+	offsetAddr := args[2].Pointer()
+	count := int64(args[3].SizeT())
+
+	inFile := t.GetFileVFS2(inFD)
+	if inFile == nil {
+		return 0, nil, syserror.EBADF
+	}
+	defer inFile.DecRef()
+	if !inFile.IsReadable() {
+		return 0, nil, syserror.EBADF
+	}
+
+	outFile := t.GetFileVFS2(outFD)
+	if outFile == nil {
+		return 0, nil, syserror.EBADF
+	}
+	defer outFile.DecRef()
+	if !outFile.IsWritable() {
+		return 0, nil, syserror.EBADF
+	}
+
+	// Verify that the outFile Append flag is not set.
+	if outFile.StatusFlags()&linux.O_APPEND != 0 {
+		return 0, nil, syserror.EINVAL
+	}
+
+	// Verify that we have a regular infile. This is a requirement; the
+	// same check appears in Linux (fs/splice.c:splice_direct_to_actor).
+	if stat, err := inFile.Stat(t, vfs.StatOptions{Mask: linux.STATX_TYPE}); err != nil {
+		return 0, nil, err
+	} else if stat.Mask&linux.STATX_TYPE == 0 || stat.Mode&linux.S_IFMT != linux.S_IFREG {
+		return 0, nil, syserror.EINVAL
+	}
+
+	// Copy offset if it exists.
+	offset := int64(-1)
+	if offsetAddr != 0 {
+		if inFile.Options().DenyPRead {
+			return 0, nil, syserror.ESPIPE
+		}
+		if _, err := t.CopyIn(offsetAddr, &offset); err != nil {
+			return 0, nil, err
+		}
+		if offset < 0 {
+			return 0, nil, syserror.EINVAL
+		}
+		if offset+count < 0 {
+			return 0, nil, syserror.EINVAL
+		}
+	}
+
+	// Validate count. This must come after offset checks.
+	if count < 0 {
+		return 0, nil, syserror.EINVAL
+	}
+	if count == 0 {
+		return 0, nil, nil
+	}
+	if count > int64(kernel.MAX_RW_COUNT) {
+		count = int64(kernel.MAX_RW_COUNT)
+	}
+
+	// Copy data.
+	outPipeFD, outIsPipe := outFile.Impl().(*pipe.VFSPipeFD)
+	nonBlock := outFile.StatusFlags()&linux.O_NONBLOCK != 0
+	var (
+		n   int64
+		err error
+	)
+	for {
+		if outIsPipe {
+			// Outfile is a pipe. Life is easy.
+			if offset != -1 {
+				n, err = inFile.PRead(t, outPipeFD.IOSequence(count), offset, vfs.ReadOptions{})
+				offset += n
+			} else {
+				n, err = inFile.Read(t, outPipeFD.IOSequence(count), vfs.ReadOptions{})
+			}
+		} else {
+			// Read inFile to a buffer then write the full contents to outFile.
+			buf := make([]byte, count)
+			if offset != -1 {
+				n, err = inFile.PRead(t, usermem.BytesIOSequence(buf), offset, vfs.ReadOptions{})
+			} else {
+				n, err = inFile.Read(t, usermem.BytesIOSequence(buf), vfs.ReadOptions{})
+			}
+			if err == io.EOF {
+				err = nil
+			}
+
+			// Write all of the bytes that we read. This may need
+			// multiple write calls to complete.
+			buf = buf[:n]
+			for len(buf) > 0 {
+				wn, werr := outFile.Write(t, usermem.BytesIOSequence(buf), vfs.WriteOptions{})
+				buf = buf[wn:]
+				if offset != -1 {
+					offset += wn
+				}
+				if werr == nil {
+					continue
+				}
+				if werr != syserror.ErrWouldBlock || nonBlock {
+					// We didn't complete the write. Only
+					// report the bytes that were actually
+					// written.
+					err = werr
+					n -= int64(len(buf))
+					break
+				}
+			}
+		}
+
+		if n != 0 || err != syserror.ErrWouldBlock || nonBlock {
+			break
+		}
+		if err = waitForBoth(t, inFile, outFile); err != nil {
+			return 0, nil, err
+		}
+	}
+
+	if offsetAddr != 0 {
+		// Copy out the new offset.
+		if _, err := t.CopyOut(offsetAddr, offset); err != nil {
+			return 0, nil, err
+		}
+	}
+
+	if n == 0 {
+		return 0, nil, err
+	}
+
+	return uintptr(n), nil, nil
+}
+
+// waitForBoth waits for inFile to be readable, and outFile to be writable.
+func waitForBoth(t *kernel.Task, inFile, outFile *vfs.FileDescription) error {
+	var (
+		inW   waiter.Entry
+		inCh  chan struct{}
+		outW  waiter.Entry
+		outCh chan struct{}
+	)
+	for {
 		if inFile.Readiness(eventMaskRead)&eventMaskRead == 0 {
 			if inCh == nil {
-				inCh = make(chan struct{}, 1)
-				inW, _ := waiter.NewChannelEntry(inCh)
+				inW, inCh = waiter.NewChannelEntry(nil)
 				inFile.EventRegister(&inW, eventMaskRead)
 				defer inFile.EventUnregister(&inW)
-				continue // Need to refresh readiness.
+				// Need to refresh readyness.
+				continue
 			}
 			if err := t.Block(inCh); err != nil {
-				return 0, nil, err
+				return err
 			}
 		}
 		if outFile.Readiness(eventMaskWrite)&eventMaskWrite == 0 {
 			if outCh == nil {
-				outCh = make(chan struct{}, 1)
-				outW, _ := waiter.NewChannelEntry(outCh)
+				outW, outCh = waiter.NewChannelEntry(nil)
 				outFile.EventRegister(&outW, eventMaskWrite)
 				defer outFile.EventUnregister(&outW)
-				continue // Need to refresh readiness.
+				// Need to refresh readyness.
+				continue
 			}
 			if err := t.Block(outCh); err != nil {
-				return 0, nil, err
+				return err
 			}
 		}
+		// Ready!
+		return nil
 	}
 }
